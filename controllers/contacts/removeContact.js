41224b5ad0556da4dce3298d5394ff0a8dde83e9
EnVy <envy7321@gmail.com>
2022-12-22 23:27:14 +0200

    save

--- controllers/contacts/removeContact.js
+++ controllers/contacts/removeContact.js
@@ -1,16 +1,12 @@
-// const Contact = require("../models/contact");
-// const { HttpError } = require("../../helpers");
+const { Contact } = require("../../models/contact");
+const { HttpError } = require("../../helpers");
 
 const remove = async (req, res, next) => {
-  //   try {
-  //     const { id } = req.params;
-  //     const result = await contacts.removeContact(id);
-  //     if (!result) {
-  //       throw HttpError(404, "Not found");
-  //     }
-  //     res.json({ message: "Delete success" });
-  //   } catch (error) {
-  //     next(error);
-  //   }
+  const { id } = req.params;
+  const result = await Contact.findByIdAndRemove(id);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json({ message: "Delete success" });
 };
 module.exports = remove;
