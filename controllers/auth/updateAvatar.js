60a0ce6a105adf02e11d126315ec63e3d122d5f1
EnVy <envy7321@gmail.com>
2023-01-13 14:01:00 +0200

    save

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,4 +1,5 @@
 const fs = require("fs/promises");
+const Jimp = require("jimp");
 const path = require("path");
 const { User } = require("../../models/user");
 
@@ -7,11 +8,20 @@ const avatarDir = path.join(__dirname, "../../", "public", "avatars");
 const updateAvatar = async (req, res) => {
   const { path: tempUpload, originalname } = req.file;
   const { _id } = req.user;
+
+  const img = await Jimp.read(`${tempUpload}`);
+  img.resize(250, 250).write(tempUpload);
+
   const fileName = `${_id}_${originalname}`;
-  const resultUpload = path.join(avatarDir, originalname);
+  const resultUpload = path.join(avatarDir, fileName);
+
   await fs.rename(tempUpload, resultUpload);
-  const avatarURL = path.join("avatars", originalname, fileName);
-  await User.findByIdAndUpdate(_id, { avatarURL });
-  res.json({ avatarURL });
+  const avatarURL = path.join("avatars", fileName);
+  await User.findByIdAndUpdate(_id, { avatarUrl: avatarURL });
+  res.json({
+    status: "Ok",
+    code: 200,
+    avatarURL,
+  });
 };
 module.exports = updateAvatar;
